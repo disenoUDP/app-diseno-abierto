8a46b50e6d07eb515d64fc5242b4f2588afcfc27
janisepulveda <janis.sepulveda@mail.udp.cl>
2023-10-06 10:39:55 -0300

    borrar titulo

--- app_testing/lib/pages/home.dart
+++ app_testing/lib/pages/home.dart
@@ -7,31 +7,67 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Desayuno',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 18,
-            fontWeight: FontWeight.bold
-          ),
+      appBar: appBar(),
+      body: Column(
+        children: [
+          TextField()
+        ],
+      ),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: const Text(
+        'Desayuno',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 18,
+          fontWeight: FontWeight.bold
         ),
-        backgroundColor: Colors.white,
-        elevation: 0.0,
-        leading: Container(
+      ),
+      backgroundColor: Colors.white,
+      elevation: 0.0,
+      leading: GestureDetector(
+        onTap: () {
+          
+        },
+        child: Container(
           margin: const EdgeInsets.all(10),
           alignment: Alignment.center,
           child: SvgPicture.asset(
             'assets/icons/Arrow - Left 2.svg',
             height: 20,
             width: 20,
+        ),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        ),
+      ),
+      
+      actions: [
+        GestureDetector(
+          onTap: () {
+
+          },
+          child: Container(
+          margin: const EdgeInsets.all(10),
+          alignment: Alignment.center,
+          width: 37,
+          child: SvgPicture.asset(
+            'assets/icons/dots.svg',
+            height: 5,
+            width: 5,
           ),
           decoration: BoxDecoration(
             color: Colors.white,
-            borderRadius: BorderRadius.circular(10),
+            borderRadius: BorderRadius.circular(10)
           ),
         ),
-      ),
+        ),
+      ],
     );
   }
 }
