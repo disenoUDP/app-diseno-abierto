cabc404090d784c1978b2a9564c306c730d392c0
janisepulveda <janis.sepulveda@mail.udp.cl>
2023-10-13 10:06:47 -0300

    subir cambios home.dart

--- app_testing/lib/pages/home.dart
+++ app_testing/lib/pages/home.dart
@@ -1,6 +1,88 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+void main() => runApp(const TabBarApp());
+
+class TabBarApp extends StatelessWidget {
+  const TabBarApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: const TabBarExample(),
+    );
+  }
+}
+
+class TabBarExample extends StatelessWidget {
+  const TabBarExample({super.key});
+
+ @override   
+  Widget build(context) {
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            'Medios de transporte',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold
+            ),
+          ),
+          bottom: const TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.directions_boat),
+              ),
+              Tab(
+                icon: Icon(Icons.directions_bus),
+              ),
+              Tab(
+                icon: Icon(Icons.directions_car),
+              ),
+            ]
+          ),
+        ),
+        body: const TabBarView(
+          children: <Widget>[
+            Center(
+              child: Text(
+                'Esto es un barco',
+                style: TextStyle(
+                  fontSize: 1,
+                  fontWeight: FontWeight.normal
+                ),
+              ),
+            ),
+            Center(
+              child: Text(
+                'Esto es un bus',
+                style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.normal
+                ),
+              ),
+            ),
+            Center(
+              child: Text(
+                'Esto es un auto',
+                style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.normal
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
@@ -8,10 +90,10 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(),
-      body: Column(
-        children: [
-          TextField()
-        ],
+      body: const Column(
+        // children: [
+        //   TextField()
+        // ],
       ),
     );
   }
@@ -19,13 +101,16 @@ class HomePage extends StatelessWidget {
   AppBar appBar() {
     return AppBar(
       title: const Text(
-        'Desayuno',
+        'Esto es una prueba',
         style: TextStyle(
           color: Colors.black,
           fontSize: 18,
           fontWeight: FontWeight.bold
         ),
       ),
+
+      // esto cambia en ios y android
+      centerTitle: true, 
       backgroundColor: Colors.white,
       elevation: 0.0,
       leading: GestureDetector(
@@ -35,14 +120,14 @@ class HomePage extends StatelessWidget {
         child: Container(
           margin: const EdgeInsets.all(10),
           alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+        ),
           child: SvgPicture.asset(
             'assets/icons/Arrow - Left 2.svg',
             height: 20,
             width: 20,
-        ),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(10),
         ),
         ),
       ),
@@ -56,14 +141,14 @@ class HomePage extends StatelessWidget {
           margin: const EdgeInsets.all(10),
           alignment: Alignment.center,
           width: 37,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10)
+          ),
           child: SvgPicture.asset(
             'assets/icons/dots.svg',
             height: 5,
             width: 5,
-          ),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(10)
           ),
         ),
         ),
