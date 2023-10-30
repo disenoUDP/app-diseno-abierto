c530624590719da0d2dcd044b841de55deeafaed
aarón montoya-moraga <montoyamoraga@gmail.com>
2023-10-30 01:17:59 -0300

    agregar ejemplo de JSON

--- mapa_diseno_abierto/lib/main.dart
+++ mapa_diseno_abierto/lib/main.dart
@@ -1,8 +1,56 @@
-// import 'dart:convert';
-// ignore: unused_import
-import 'package:mapa_diseno_abierto/pages/home.dart';
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+// import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter/material.dart';
-// import 'package:flutter/services.dart' show rootBundle;
+import 'package:http/http.dart' as http;
+
+// // import 'dart:convert';
+// import 'package:flutter/material.dart';
+// // import 'package:flutter/services.dart' show rootBundle;
+
+Future<List<Taller>> fetchTalleres(http.Client client) async {
+  final response = await client
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
+
+  // Use the compute function to run parseTalleres in a separate isolate.
+  return compute(parseTalleres, response.body);
+}
+
+// A function that converts a response body into a List<Taller>.
+List<Taller> parseTalleres(String responseBody) {
+  final parsed =
+      (jsonDecode(responseBody) as List).cast<Map<String, dynamic>>();
+
+  return parsed.map<Taller>((json) => Taller.fromJson(json)).toList();
+}
+
+class Taller {
+  final int albumId;
+  final int id;
+  final String title;
+  final String url;
+  final String thumbnailUrl;
+
+  const Taller({
+    required this.albumId,
+    required this.id,
+    required this.title,
+    required this.url,
+    required this.thumbnailUrl,
+  });
+
+  factory Taller.fromJson(Map<String, dynamic> json) {
+    return Taller(
+      albumId: json['albumId'] as int,
+      id: json['id'] as int,
+      title: json['title'] as String,
+      url: json['url'] as String,
+      thumbnailUrl: json['thumbnailUrl'] as String,
+    );
+  }
+}
 
 void main() {
   runApp(const MyApp());
@@ -14,135 +62,97 @@ class MyApp extends StatelessWidget {
   // this widget is the root of your application
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: const TabBarApp(),
-      theme: ThemeData(fontFamily: 'Popppins', useMaterial3: true),
+    const appTitle = 'demo';
+
+    return const MaterialApp(
+      // theme: ThemeData(useMaterial3: true),
+      title: appTitle,
+      home: MyHomePage(title: appTitle),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
 
-// void main() {
-//   runApp(const MyApp());
-// }
+  final String title;
+  @override
+  Widget build(context) {
+    return DefaultTabController(
+      initialIndex: 0,
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            '#disenoabiertoudp',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          bottom: const TabBar(tabs: <Widget>[
+            Tab(
+              icon: Icon(Icons.maps_home_work_outlined),
+            ),
+            Tab(
+              icon: Icon(Icons.map_outlined),
+            ),
+            Tab(
+              icon: Icon(Icons.checklist_outlined),
+            ),
+          ]),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            FutureBuilder<List<Taller>>(
+              future: fetchTalleres(http.Client()),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return const Center(
+                    child: Text('An error has occurred!'),
+                  );
+                } else if (snapshot.hasData) {
+                  return TalleresList(talleres: snapshot.data!);
+                } else {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              },
+            ),
+            const Center(
+              child: Text(
+                'Mapa',
+                style: TextStyle(fontSize: 17, fontWeight: FontWeight.normal),
+              ),
+            ),
+            const Center(
+              child: Text(
+                'Talleres',
+                style: TextStyle(fontSize: 17, fontWeight: FontWeight.normal),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
+class TalleresList extends StatelessWidget {
+  const TalleresList({super.key, required this.talleres});
 
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         // This is the theme of your application.
-//         //
-//         // TRY THIS: Try running your application with "flutter run". You'll see
-//         // the application has a blue toolbar. Then, without quitting the app,
-//         // try changing the seedColor in the colorScheme below to Colors.green
-//         // and then invoke "hot reload" (save your changes or press the "hot
-//         // reload" button in a Flutter-supported IDE, or press "r" if you used
-//         // the command line to start the app).
-//         //
-//         // Notice that the counter didn't reset back to zero; the application
-//         // state is not lost during the reload. To reset the state, use hot
-//         // restart instead.
-//         //
-//         // This works for code too, not just values: Most code changes can be
-//         // tested with just a hot reload.
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         useMaterial3: true,
-//       ),
-//       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-//     );
-//   }
-// }
+  final List<Taller> talleres;
 
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
-
-//   // This widget is the home page of your application. It is stateful, meaning
-//   // that it has a State object (defined below) that contains fields that affect
-//   // how it looks.
-
-//   // This class is the configuration for the state. It holds the values (in this
-//   // case the title) provided by the parent (in this case the App widget) and
-//   // used by the build method of the State. Fields in a Widget subclass are
-//   // always marked "final".
-
-//   final String title;
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-//       // This call to setState tells the Flutter framework that something has
-//       // changed in this State, which causes it to rerun the build method below
-//       // so that the display can reflect the updated values. If we changed
-//       // _counter without calling setState(), then the build method would not be
-//       // called again, and so nothing would appear to happen.
-//       _counter++;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // This method is rerun every time setState is called, for instance as done
-//     // by the _incrementCounter method above.
-//     //
-//     // The Flutter framework has been optimized to make rerunning build methods
-//     // fast, so that you can just rebuild anything that needs updating rather
-//     // than having to individually change instances of widgets.
-//     return Scaffold(
-//       appBar: AppBar(
-//         // TRY THIS: Try changing the color here to a specific color (to
-//         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-//         // change color while the other colors stay the same.
-//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-//         // Here we take the value from the MyHomePage object that was created by
-//         // the App.build method, and use it to set our appbar title.
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         // Center is a layout widget. It takes a single child and positions it
-//         // in the middle of the parent.
-//         child: Column(
-//           // Column is also a layout widget. It takes a list of children and
-//           // arranges them vertically. By default, it sizes itself to fit its
-//           // children horizontally, and tries to be as tall as its parent.
-//           //
-//           // Column has various properties to control how it sizes itself and
-//           // how it positions its children. Here we use mainAxisAlignment to
-//           // center the children vertically; the main axis here is the vertical
-//           // axis because Columns are vertical (the cross axis would be
-//           // horizontal).
-//           //
-//           // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-//           // action in the IDE, or press "p" in the console), to see the
-//           // wireframe for each widget.
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headlineMedium,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+      ),
+      itemCount: talleres.length,
+      itemBuilder: (context, index) {
+        return Image.network(talleres[index].thumbnailUrl);
+      },
+    );
+  }
+}
