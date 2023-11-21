1afd5f42b100195c2c963daaf576f21c37a0e9de
aarón montoya-moraga <montoyamoraga@gmail.com>
2023-11-21 03:12:49 -0300

    borrar mas comentarios

---
+++ diseno_abierto/lib/archivos.dart
@@ -0,0 +1,11 @@
+const talleresJSON =
+    "https://raw.githubusercontent.com/disenoUDP/assets/main/diseno-abierto/talleres.json";
+
+const imagenPlanta1 =
+    "https://raw.githubusercontent.com/disenoUDP/assets/main/diseno-abierto/mapa/planta1.png";
+
+const imagenPlanta2 =
+    "https://raw.githubusercontent.com/disenoUDP/assets/main/diseno-abierto/mapa/planta2.png";
+
+const imagenPlanta3 =
+    "https://raw.githubusercontent.com/disenoUDP/assets/main/diseno-abierto/mapa/planta3.png";
--- diseno_abierto/lib/main.dart
+++ diseno_abierto/lib/main.dart
@@ -1,11 +1,11 @@
+import 'archivos.dart' as archivos;
 import 'estilo.dart' as estilo;
 import 'inicio.dart' as inicio;
-import 'taller.dart' as taller;
+import 'taller_prob.dart' as taller;
 
 import 'dart:async';
 import 'dart:convert';
 // import 'dart:ffi';
-// import 'dart:ui';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 // import 'package:flutter/services.dart';
@@ -21,8 +21,7 @@ import 'package:url_launcher/url_launcher.dart';
 // }
 
 Future<List<taller.Taller>> fetchTalleres(http.Client client) async {
-  final response = await client.get(Uri.parse(
-      'https://raw.githubusercontent.com/disenoUDP/assets/main/diseno-abierto/talleres.json'));
+  final response = await client.get(Uri.parse(archivos.talleresJSON));
   if (response.statusCode == 200) {
     // If the server did return a 200 OK response, then parse the JSON.
     //return Taller.fromJson(jsonDecode(response.body));
@@ -123,17 +122,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  // void _incrementCounter() {
-  //   setState(() {
-  //     // This call to setState tells the Flutter framework that something has
-  //     // changed in this State, which causes it to rerun the build method below
-  //     // so that the display can reflect the updated values. If we changed
-  //     // _counter without calling setState(), then the build method would not be
-  //     // called again, and so nothing would appear to happen.
-  //     _counter++;
-  //   });
-  // }
-
   @override
   Widget build(BuildContext context) {
     const colorSubheader = Color(0xFF005EB8);
@@ -281,9 +269,7 @@ class _MyHomePageState extends State<MyHomePage> {
                                       placeholder: kTransparentImage,
                                       width: 540,
                                       height: 540,
-                                      image:
-                                          'https://raw.githubusercontent.com/disenoUDP/assets/main/diseno-abierto/mapa/planta1.png'),
-
+                                      image: archivos.imagenPlanta1),
                                   // primer texto
                                   const Positioned(
                                     top: 70,
@@ -303,9 +289,7 @@ class _MyHomePageState extends State<MyHomePage> {
                                       placeholder: kTransparentImage,
                                       width: 400,
                                       height: 400,
-                                      image:
-                                          'https://raw.githubusercontent.com/disenoUDP/assets/main/diseno-abierto/mapa/planta2.png'),
-
+                                      image: archivos.imagenPlanta2),
                                   // segundo texto
                                   const Positioned(
                                     top: 500,
@@ -325,9 +309,7 @@ class _MyHomePageState extends State<MyHomePage> {
                                       placeholder: kTransparentImage,
                                       width: 400,
                                       height: 400,
-                                      image:
-                                          'https://raw.githubusercontent.com/disenoUDP/assets/main/diseno-abierto/mapa/planta3.png'),
-
+                                      image: archivos.imagenPlanta3),
                                   // tercer texto
                                   const Positioned(
                                     top: 780,
@@ -364,7 +346,6 @@ class _MyHomePageState extends State<MyHomePage> {
                   );
                 } else if (snapshot.hasData) {
                   //return TalleresList(talleres: snapshot.data!);
-                  // return const Text("bla");
                   return Stack(
                     children: [
                       TalleresList(talleres: snapshot.data!),
@@ -405,54 +386,6 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ],
         ),
-        // Center(
-        //   // Center is a layout widget. It takes a single child and positions it
-        //   // in the middle of the parent.
-        //   child: Column(
-        //     // Column is also a layout widget. It takes a list of children and
-        //     // arranges them vertically. By default, it sizes itself to fit its
-        //     // children horizontally, and tries to be as tall as its parent.
-        //     //
-        //     // Column has various properties to control how it sizes itself and
-        //     // how it positions its children. Here we use mainAxisAlignment to
-        //     // center the children vertically; the main axis here is the vertical
-        //     // axis because Columns are vertical (the cross axis would be
-        //     // horizontal).
-        //     //
-        //     // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-        //     // action in the IDE, or press "p" in the console), to see the
-        //     // wireframe for each widget.
-        //     mainAxisAlignment: MainAxisAlignment.center,
-        //     children: <Widget>[
-        //       Container(
-        //         alignment: Alignment.center,
-        //         width: 0.5 * MediaQuery.of(context).size.width,
-        //         height: 0.5 *
-        //             (MediaQuery.of(context).size.height -
-        //                 MediaQuery.of(context).padding.top -
-        //                 MediaQuery.of(context).padding.bottom),
-        //         // height: 0.5 * MediaQuery.of(context).size.height,
-        //         // height: 0.25 * View.of(context).physicalSize.height,
-        //         child: FadeInImage.memoryNetwork(
-        //             fit: BoxFit.cover,
-        //             placeholder: kTransparentImage,
-        //             image:
-        //                 'https://raw.githubusercontent.com/disenoUDP/assets/main/diseno-abierto/splash.png'),
-        //       ),
-        //       // FadeInImage.memoryNetwork(
-        //       //     fit: BoxFit.contain,
-        //       //     placeholder: kTransparentImage,
-        //       //     image:
-        //       //         'https://raw.githubusercontent.com/disenoUDP/assets/main/diseno-abierto/logo-escuela.png'),
-        //       Text('#disenoabiertoudp',
-        //           style: Theme.of(context).textTheme.labelMedium),
-        //       Text('2023 diciembre 11-15',
-        //           style: Theme.of(context).textTheme.labelMedium),
-        //       Text('salvador sanfuentes 2221',
-        //           style: Theme.of(context).textTheme.labelMedium),
-        //     ],
-        //   ),
-        // ),
       ),
     );
   }
