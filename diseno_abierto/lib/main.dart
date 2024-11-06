7ea51b7b47bd441c42d5077104395d83e652d851
aarón montoya-moraga <montoyamoraga@gmail.com>
2024-11-06 16:02:49 -0300

    borrar llamado a biblioteca nfc

--- diseno_abierto/lib/main.dart
+++ diseno_abierto/lib/main.dart
@@ -4,7 +4,6 @@ import 'inicio.dart' as inicio;
 import 'mapa.dart' as plantas;
 import 'proyectos.dart' as proyectos;
 import 'talleres.dart' as talleres;
-import 'nfc.dart' as nfc;
 import 'package:flutter/material.dart';
 
 void main() {
